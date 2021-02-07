2019be481f5374964a553cbfa8f301546c2c4333
Pahud Hsieh <pahudnet@gmail.com>
2021-02-07 11:15:29 +0000

    remove .github/dependabot.yml

--- .projenrc.js
+++ .projenrc.js
@@ -80,7 +80,7 @@ workflow.addJobs({
 });
 
 
-const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
+const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log', 'dependabot.yml'];
 project.npmignore.exclude(...common_exclude);
 project.gitignore.exclude(...common_exclude);
 
