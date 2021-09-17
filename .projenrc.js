760b1e58f351c257e6b09c6e3e2179c81af3e4c3
Pahud Hsieh <pahudnet@gmail.com>
2021-09-17 23:23:54 +0800

    bump set-value (#139)

--- .projenrc.js
+++ .projenrc.js
@@ -22,7 +22,6 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/aws-secretsmanager',
     '@aws-cdk/core',
   ],
-  minNodeVersion: '14.17.0',
   depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
     ignoreProjen: false,
     workflowOptions: {
@@ -51,7 +50,8 @@ const project = new AwsCdkConstructLibrary({
 });
 
 project.package.addField('resolutions', {
-  'pac-resolver': '5.0.0',
+  'pac-resolver': '^5.0.0',
+  'set-value': '^4.0.1',
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log', 'dependabot.yml'];
